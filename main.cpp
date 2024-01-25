e65f6328e58d8a214680aba3e41533d8f35919e4
celerizer <33245078+celerizer@users.noreply.github.com>
2024-01-25 11:16:36 -0600

    Feature: Launching from CL-web via URI protocol

--- main.cpp
+++ main.cpp
@@ -1,16 +1,41 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QMessageBox>
+#include <QCommandLineParser>
+#include <QUrlQuery>
+
 #include <QRetro.h>
 
 int main(int argc, char *argv[])
 {
+  QCoreApplication app(argc, argv);
+  QCoreApplication::setApplicationName("Pleasant");
   QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
-  QApplication a(argc, argv);
+
+  QCommandLineParser parser;
+  QCommandLineOption option_uri(QStringList() << "uri", "URI to process", "uri");
+  parser.addOption(option_uri);
+  parser.process(app);
+
+  QUrl url(parser.value(option_uri));
+  if (url.isValid())
+  {
+    QString command = url.host();
+
+    if (command == "launch")
+    {
+      QUrlQuery query(url);
+      auto game_id = query.queryItemValue("game_id").toUInt();
+
+      if (game_id)
+      {
+
+      }
+    }
+  }
 
   MainWindow w;
   w.show();
 
-  return a.exec();
+  return app.exec();
 }
